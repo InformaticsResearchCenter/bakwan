dbb20292cba6cd8346041d901ee8815d37edf17a
trianggadios <trianggadio@gmail.com>
2019-09-25 12:36:27 +0700

    up

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import bakwan
 import balabala
 
-# kerupuk = balabala.BalaBala()
-# kerupuk.sendMessageWithoutAPI("1184011", "ayo dong bimbingan biar cepet")
+kerupuk = balabala.BalaBala()
+kerupuk.sendMessageWithoutAPI("1184011", "ayo dong bimbingan biar cepet")
 
 # tempe = bakwan.Bakwan()
 # tempe.sendMessageWithAPI("6282217401448", "Hayo bimbingan")
