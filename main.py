c71a4a935ed0a15971dfd53d353fed94322ea84b
trianggadios <trianggadio@gmail.com>
2019-09-25 09:44:07 +0700

    update main tinggal nambahin bypass qrcodenya

--- main.py
+++ main.py
@@ -4,58 +4,52 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 
+class Send:
 
-options = webdriver.ChromeOptions()
-options.add_argument('--user-data-dir=./user_data')
-driver = webdriver.Chrome(chrome_options=options)
+    driver = ''
+    wait   = ''
 
-driver.get("https://web.whatsapp.com/")
+    def __init__(self):
+        self.driver = webdriver.Chrome('/home/burger-man/Downloads/chromedriver')
+        self.wait = WebDriverWait(self.driver, 600)
+        self.driver.get("https://web.whatsapp.com")
+        target = '"_3RWII"'
+        x_arg = '//div[contains(@class, ' + target + ')]'
+        group_title = self.wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
+        print (group_title)
+        print("Tunggu bentaran!")
 
-wait = WebDriverWait(driver, 600)
+    def sendMessage(self, contact, message):
 
-string = 'Anda belum bimbingan'
-
-target = '"_3RWII"'
-
-x_arg = '//div[contains(@class, ' + target + ')]'
-group_title = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
-print (group_title)
-print("Tunggu bentaran!")
-
-new_chat = driver.find_elements_by_tag_name("span")[12].get_attribute("data-icon")
-
-chat = driver.find_elements_by_tag_name("span")[12]
-chitchat = chat.get_attribute("data-icon")
-if chitchat == "chat":
-    chat.click()
+        chat = self.driver.find_elements_by_tag_name("span")[12]
+        chitchat = chat.get_attribute("data-icon")
+        if chitchat == "chat":
+            chat.click()
 
 
-#search target
-search_chat = driver.find_element_by_tag_name("input").get_attribute("title")
+        search = self.driver.find_element_by_tag_name("input")
+        sirchsearch =  search.get_attribute("title")
+        if sirchsearch == "Search contacts":
+            search.click()
+            search.send_keys(contact)
 
-search = driver.find_element_by_tag_name("input")
-sirchsearch =  search.get_attribute("title")
-if sirchsearch == "Search contacts":
-    search.click()
-    search.send_keys("Dezha Polpos")
+        time.sleep(1)
 
-time.sleep(1)
+        npm_target = '"'+contact+'"'
 
-npm_target = '"Dezha Polpos"'
+        target_npm = '//span[contains(@title,' + npm_target + ')]'
+        target_npm_click = self.wait.until(EC.presence_of_element_located((By.XPATH, target_npm)))
+        target_npm_click.click()
 
-#click target
-target_npm = '//span[contains(@title,' + npm_target + ')]'
-target_npm_click = wait.until(EC.presence_of_element_located((By.XPATH, target_npm)))
-target_npm_click.click()
+        time.sleep(1)
 
-time.sleep(1)
+        message_target = self.driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
+        message_target.send_keys(message)
 
-#write message
-message = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
-message.send_keys(string)
+        time.sleep(1)
 
-time.sleep(1)
+        sendbutton = self.driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')[0]
+        sendbutton.click()
 
-#send message
-sendbutton = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')[0]
-sendbutton.click()
+sending = Send()
+sending.sendMessage("1184011", "Hey, kamu bimbingan dulu")
